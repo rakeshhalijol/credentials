40bd1a10fc3f9fd63e5cf8ce3400ce1cd21cc327
rakeshhalijol <121545747+rakeshhalijol@users.noreply.github.com>
2023-04-26 11:16:15 +0530

    Update interaction.py

--- credentials-api/src/database/interaction.py
+++ credentials-api/src/database/interaction.py
@@ -4,11 +4,11 @@ from .connection import (
 )
 import json
 from src.config import MONGO_CREDENTIALS_PATH
-with open(MONGO_CREDENTIALS_PATH, "r") as f:
-    credentials: dict = json.load(f)
-user = credentials.get("user")
-password = credentials.get("password")
-print(user, password)
+# with open(MONGO_CREDENTIALS_PATH, "r") as f:
+#     credentials: dict = json.load(f)
+# user = credentials.get("user")
+# password = credentials.get("password")
+# print(user, password)
 client = connect(
     f"mongodb+srv://rakeshhalijol:Rakesh123@cluster0.inydvtx.mongodb.net/test")
 db = create_database(client=client, db_name="NATIVE-DATABASE")
